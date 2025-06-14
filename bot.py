338b95a15e38ff93a92c3bb1cd392f5109d9d231
YU-LIN, CHEN <43955193+rain-u0@users.noreply.github.com>
2025-06-14 23:25:20 +0800

    test py

--- bot.py
+++ bot.py
@@ -31,10 +31,11 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    line_bot_api.reply_message(
-        event.reply_token,
-        TextSendMessage(text=event.message.text)
-    )
+    if event.message.text.strip().lower() == "test":
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text="test")
+        )
 
 if __name__ == "__main__":
     app.run(port=5000)
